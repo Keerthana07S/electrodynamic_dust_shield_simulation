2b9f9caab02457131b629885687370399fa299ad
Keerthana-Srnivasan <140654812+Keerthana-Srnivasan@users.noreply.github.com>
2024-10-04 12:10:04 -0400

    Update eds_simulations.jl

--- eds_simulations.jl
+++ eds_simulations.jl
@@ -34,10 +34,14 @@ volume_fraction = 10 / 150 #fraction of cnts in total solution
 #permittivity of coating
 epsilon_p = complex_e_m * (1 + (3 * volume_fraction * (complex_e_cnt - complex_e_m)) / (complex_e_cnt + 2 * complex_e_m - volume_fraction * (complex_e_cnt - complex_e_m)))
 
+#time particle motion is recorded is over 0-1 seconds with 0.01 second time steps. 
 time_range = 0:0.01:1.0
 dt = 0.01
 
-#voltage is pulsating dc current running in phases with 300 ms phase delay
+#=voltage is pulsating dc current running in phases with 300 ms phase delay. 
+frequency of each phase of pulsating voltage is 1111 Hz.
+each pulse is added together to take constructive and destructive interference into account
+=#
 function voltage_phases(w, t, V_max)
     return (V_max * sign(cos(w * t)) + 300) + (V_max * sign(cos(w * t + 2 * pi / 3)) + 300) + (V_max * sign(cos(w * t + 4 * pi / 3)) + 300)
 end
@@ -77,45 +81,48 @@ function greenfunction(x, y, x_i, y_i, x_j, y_j, x_k, y_k, t, w, V_max)
     r5 = sqrt((x-x_j).^2 + (y+y_j).^2)
     r6 = sqrt((x-x_k).^2 + (y+y_k).^2)
 
-    #voltage scaling
+    #voltage scaling to take into account the pulsating 3 phase voltage
     G_13 = (1/(2*pi))*log(1/r1*r2*r3*r4*r5*r6) * voltage_phases(w, t, V_max)
     G_2 = ((1/(2*pi)))*log((r1*r2*r3)/r4*r5*r6) * voltage_phases(w, t, V_max)
 
     return G_13, G_2
 end
 
+#empty arrays to fill during for loops
 accel_x_df = Float64[]
 accel_y_df = Float64[]
 vel_x_df = Float64[]
 vel_y_df = Float64[]
 
+#calculate the acceleration of lunar dust in the x direction
 function accelerationx(q, t, E_x_i, m)
     a_x_i = q*(E_x_i*cos(2*pi*f*t))*(1/m)
     push!(accel_x_df, a_x_i) 
     return a_x_i
 end
 
+#calculate the acceleration of lunar dust in the y direction
 function accelerationy(q, t, E_y_i, m, gravity)
     a_y_i = q*(E_y_i*cos(2*pi*f*t))*(1/m) - gravity
     push!(accel_y_df, a_y_i)
     return a_y_i
 end
 
-function sort_coordinates(xboundary, yboundary)
-    xy_tuples = collect(zip(xboundary, yboundary))
-    sorted_xy = sort(xy_tuples, by = first) 
-    x_sorted, y_sorted = unzip(sorted_xy)    
-    return x_sorted, y_sorted
-end
-
+#=
+runge kutta is used to integrate values over time steps. 
+in this scenario, the fourth order runge kutta method is used to approximate velocity based on the acceleration
+in the x and y directions. 
+=#
 function runge_kutta(q, t, E_x_i, E_y_i, gravity, m, delta_t, v_x_i, v_y_i)
+    #approximate velocity in the x direction
     fivx = accelerationx(q, t, E_x_i, m)
     fiivx = accelerationx(q, t+(delta_t/2), E_x_i+(fivx*delta_t/2), m)
     fiiivx = accelerationx(q, t+(delta_t/2), E_x_i+(fiivx*delta_t/2), m)
     fivvx = accelerationx(q, t+delta_t, E_x_i+(fiiivx*delta_t), m)
     
     v_x_i = v_x_i + (1/6)*(fivx + 2*fiivx + 2*fiiivx + fivvx)
-    
+
+    #approximate velocity in the y direction
     fivy = accelerationy(q, t, E_y_i, m, gravity)
     fiivy = accelerationy(q, t+(delta_t/2), E_y_i+(fivy*delta_t/2), m, gravity)
     fiiivy = accelerationy(q, t+(delta_t/2), E_y_i+(fiivy*delta_t/2), m, gravity)
@@ -129,48 +136,43 @@ function runge_kutta(q, t, E_x_i, E_y_i, gravity, m, delta_t, v_x_i, v_y_i)
     return v_x_i, v_y_i
 end
 
-function unzip(iterable)
-    return map(x -> x[1], iterable), map(x -> x[2], iterable)
-end
-
-function ∂G13_∂x(G_13, x)
-    return (-1).*ForwardDiff.derivative(x-> G_13, x)
-end
-function ∂G13_∂y(G_13, y)
-    return (-1).*ForwardDiff.derivative(y-> G_13, y)
-end
-function ∂G2_∂x(G_2, x)
-    return (-1).*ForwardDiff.derivative(x-> G_2, x)
-end
-function ∂G2_∂y(G_2, y)
-    return (-1).*ForwardDiff.derivative(y-> G_2, y)
-end
-
+#initial values for velocity of particle in the x and y directions
 global v_x_i = 0.0
 global v_y_i = 0.0
 
+#initial arrays for electric field in the x and y directions
 E_x = Float64[]
 E_y = Float64[]
 
+#=loop through all times from 0-1 seconds in 0.01 second time steps. 
+for each time, assess the position, velocity, and acceleration of the particle
+=#
 for time in time_range
-    m = ((4/3) * pi * R.^3) * 3000
-    coulomb_force = q * ((voltage_phases(w, time, V_max)) / d) * cos(w * time)
-    gravity = m .* 1.625
-    v_drag = 6 .* pi .* R .* 1.81e-5 .* v_y_i
-    image_adhesion = q^2 / (16 * pi * epsilon_s * epsilon_p * (0.0001 + R)^2)
+    m = ((4/3) * pi * R.^3) * 3000 #mass of lunar dust particle
+    coulomb_force = q * ((voltage_phases(w, time, V_max)) / d) * cos(w * time) #coulomb force
+    gravity = m .* 1.625 #gravitational acceleration
+    v_drag = 6 .* pi .* R .* 1.81e-5 .* v_y_i #viscous drag force
+    image_adhesion = q^2 / (16 * pi * epsilon_s * epsilon_p * (0.0001 + R)^2) #image adhesion force
 
     for i in 1:div(n_segments, 3)
-        x_i, y_i = xboundary_ea[i], yboundary_ea[i]
-        x_j, y_j = xboundary_eb[i], yboundary_eb[i]
-        x_k, y_k = xboundary_ec[i], yboundary_ec[i]
+        #x and y coordinates of each electrode in Archimedean spiral segments
+        x_i, y_i = xboundary_ea[i], yboundary_ea[i] #i is electrode 1
+        x_j, y_j = xboundary_eb[i], yboundary_eb[i] #j is electrode 2
+        x_k, y_k = xboundary_ec[i], yboundary_ec[i] #k is electrode 3
 
+        #odd phases are calculated separately from even phases due to differences in the electric field magnitude
         G_13, G_2 = greenfunction(x_P, y_P, x_i, y_i, x_j, y_j, x_k, y_k, time, w, V_max)
 
         push!(G_phase13, G_13)
         push!(G_phase2, G_2)
     end
-
+    
+    #=the number of line segments for each electrode is 8.
+    the number of values in each array after calculating the finite differences will be one less than the 
+    number of line segments. 
+    =#
     for i in 1:7
+        #use finite differences to find the partial derivatives of the green's function values
         ∂G13x =(G_phase13[i + 1] - G_phase13[i]) / (xboundary_ea[i + 1] - xboundary_ea[i])
         ∂G13y =(G_phase13[i + 1] - G_phase13[i]) / (yboundary_ea[i + 1] - yboundary_ea[i])
         ∂G2x = (G_phase2[i + 1] - G_phase2[i]) / (xboundary_eb[i + 1] - xboundary_eb[i])
@@ -187,15 +189,27 @@ for time in time_range
         push!(E_y, E_y_i)
     end
 
+    #electric field magnitude
     E2 = sqrt.(E_x .^ 2 .+ E_y .^ 2) 
 
+    #dielectrophoretic force
     dielectrophoretic_force = 2 * pi * R.^3 * ((epsilon_p .- epsilon_s) / (epsilon_p .+ 2 * epsilon_s)) .* E2
 
+    #=calculate net force (N) as the coulomb and dielectrophoretic forces 
+    force the particle upwards, while image adhesion, gravitational acceleration, and 
+    viscous drag force the particle downwards.
+    =#
     F = coulomb_force .+ dielectrophoretic_force .- image_adhesion .- gravity .-v_drag
 
+     #=the number of line segments for each electrode is 8.
+    the number of values in each array after calculating the finite differences will be one less than the 
+    number of line segments. 
+    =#
     for i in 1:7
         global v_x_i, v_y_i = runge_kutta(q, time, E_x[i], E_y[i], gravity, m, dt, v_x_i, v_y_i)
     end
+
+    #find the average of the values in each array to get the general value across the electrode's area
     average_F = mean(F)
     average_accelx = mean(accel_x_df)
     average_accely = mean(accel_y_df)
